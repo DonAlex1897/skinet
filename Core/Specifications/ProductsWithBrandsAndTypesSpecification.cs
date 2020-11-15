bfb5bdd112722b2983d3d58ff5f066ab594865d4
DonAlex <kaviyani.ma@gmail.com>
2020-11-15 07:59:47 +0330

    End Of Section 6

--- Core/Specifications/ProductsWithBrandsAndTypesSpecification.cs
+++ Core/Specifications/ProductsWithBrandsAndTypesSpecification.cs
@@ -6,10 +6,33 @@ namespace Core.Specifications
 {
     public class ProductsWithBrandsAndTypesSpecification : BaseSpecification<Product>
     {
-        public ProductsWithBrandsAndTypesSpecification()
+        public ProductsWithBrandsAndTypesSpecification(ProductSpecParams specParams)
+            : base(x => (
+                (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
+                (!specParams.BrandId.HasValue || x.ProductBrandId == specParams.BrandId) &&
+                (!specParams.TypeId.HasValue || x.ProductTypeId == specParams.TypeId)
+            ))
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(specParams.PageSize * (specParams.PageIndex - 1), specParams.PageSize);
+
+            if(!string.IsNullOrEmpty(specParams.Sort))
+            {
+                switch (specParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(x => x.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderBy(x => x.Price);
+                        break;
+                    default:
+                        AddOrderBy(x => x.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithBrandsAndTypesSpecification(int id) : base(x => x.Id == id)
--- Infrastructure/Data/Repositories/GenericRepository.cs
+++ Infrastructure/Data/Repositories/GenericRepository.cs
@@ -37,9 +37,14 @@ namespace Infrastructure.Data.Repositories
             return await ApplySpecification(spec).ToListAsync();
         }
 
+        public Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return ApplySpecification(spec).CountAsync();
+        }
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
+
     }
 }
