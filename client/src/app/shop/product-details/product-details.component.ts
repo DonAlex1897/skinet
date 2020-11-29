0ff5f00a3e8b72216c03d534acc3094705b1a0f5
DonAlex <kaviyani.ma@gmail.com>
2020-11-30 01:04:58 +0330

    End of Adding Home Page Slides

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -12,8 +13,9 @@ export class ProductDetailsComponent implements OnInit {
   product!: IProduct;
   _activateRoute: string | null;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) {
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) {
     this._activateRoute = this.activateRoute.snapshot.paramMap.get('id');
+    this.bcService.set('@productName', ' ');
   }
 
   ngOnInit(): void {
@@ -23,6 +25,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     this.shopService.getProduct(+(this._activateRoute ? this._activateRoute : '0')).subscribe(response => {
       this.product = response;
+      this.bcService.set('@productName', this.product.name);
     }, error => {
       console.log(error);
     })
